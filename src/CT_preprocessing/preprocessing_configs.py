8205507eda4537f8301a306d7b107f727c74f0b3
tumeng0302 <wilson.tu46@gmail.com>
2025-09-22 23:43:59 +0800

    config update

--- src/CT_preprocessing/preprocessing_configs.py
+++ src/CT_preprocessing/preprocessing_configs.py
@@ -12,7 +12,7 @@ class DataInfoPipelineConfig:
 class EqualizeConfig:
     _value_range: tuple[int, int]
     kernel_size_ratio: float = 1/16
-    clip_limit: float = field(init=False, default=0.03)
+    clip_limit: float = 0.005
     nbins: int | None = field(init=False, default=None)
     def __post_init__(self,):
         self.nbins = max(self._value_range) - min(self._value_range)
@@ -22,15 +22,21 @@ class SlicePreprocessingConfig:
     raw_data_path: Path = DataInfoPipelineConfig.raw_data_path
     label_data_path: Path = DataInfoPipelineConfig.label_data_path
     save_data_path: Path = Path("./data_folder/processed_data/first_version").absolute()
-    num_producer: int = 2
+
+    num_producer: int = 1
     num_producer_thread: int = 4
     num_consumer: int = 1
+    max_dicom_queue: int = 1
+
     target_slice_size: tuple[int, int] = (512, 512)
-    value_range: tuple[int, int] = (-1024, 1024)
-    align_slice_index: tuple[int] = tuple({1, 5, 7})
+    value_range: tuple[int, int] = (100, 1024)
+    export_value_range: tuple[int, int] = (-1024, 1024)
+    align_slice_percentile: tuple[int] = tuple({5, 25, 50, 75, 95})
     padding_value: int = 0
     split_num: int = 5
+    top_k_offset: int = 2
     _equalize_config = EqualizeConfig(value_range)
+    
     def __post_init__(self,):
         image_tr_path = self.save_data_path.joinpath("imagesTr")
         image_ts_path = self.save_data_path.joinpath("imagesTs")
@@ -38,6 +44,10 @@ class SlicePreprocessingConfig:
         os.makedirs(image_tr_path, exist_ok=True)
         os.makedirs(image_ts_path, exist_ok=True)
         os.makedirs(label_path, exist_ok=True)
+        if self.top_k_offset > len(self.align_slice_percentile):
+            raise ValueError(
+                f"top_k_offset should be greater than len(align_slice_percentile): {len(self.align_slice_percentile)}"
+            )
 
     @property
     def equalize_config(self,):
